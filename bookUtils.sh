135bcfd4df5fdcec2c0df81a882e66d6aa921821
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2020-02-24 21:31:09 +0100

    generalize md transform + epub creat

--- bookUtils.sh
+++ bookUtils.sh
@@ -2,8 +2,7 @@
 
 ts=$(date +%s)
 WORK=/tmp/booksAlive$ts
-if [ ! -z "$1" ]
-  then
+if [ ! -z "$1" ];then
     WORK=$1
 fi
 echo "0. working with $WORK"
@@ -29,7 +28,8 @@ function extractPdfImages(){
 textdir="$WORK/text"
 function ocrFractured(){
     LANG="deu_frak"
-    if [ ! -z "$1" ]; then
+    if [ ! -z "$1" ]
+      then
         LANG=$1
     fi
     # recognize fractured german text in raw images:
@@ -37,7 +37,8 @@ function ocrFractured(){
       sudo apt install -y tesseract-ocr tesseract-ocr-deu-frak
     fi
     mkdir -p "$textdir"
-    for img in `ls -v $pagesdir/page*.png`
+    echo "scanning $pagesdir"
+    for img in `ls -v $pagesdir/*.png`
     do
         png=`basename $img`
         page=${png:0:-4} #no-file-ext
@@ -56,26 +57,76 @@ function collectMarkdown()
     book=$bookName.md
     mkdir -p "$bookdir"
     bookfile="$bookdir/$book"
-    for text in `ls -v $textdir/*.md`
+    for text in `ls -v $textdir/*/*.md`
     do
         #skip empty files...
         echo "processing "+$text
-        echo "<div id=$text>" >> "$bookfile"
+        #echo "<div id=\"$text\">" >> "$bookfile"
         cat $text >> "$bookfile"
-        echo "</div>" >> "$bookfile"
+        #echo "</div>" >> "$bookfile"
     done
     echo "created $book"
 }
 
+function transformMarkdown()
+{
+    MD_FIND='*.md'
+    if [ ! -z "$1" ]; then
+        MD_FIND=$1
+    fi
+    
+    tdir="$DIR/trans"
+    rm -r -v "$tdir"
+    mkdir -p "$tdir"
+    rm "$bookfile"
+
+    DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+    cd "$textdir"
+    for text in `ls -v ${MD_FIND}`; do
+        echo "processing "$text
+
+        parent=$(dirname "$text")
+        mkdir -p "$tdir/$parent"
+        target="$tdir/$text"
+
+        file=$(basename $text)
+        cleaned=$(find "$DIR/clean" -name "$file")
+        if ! [ -z "$cleaned" ]; then
+            text="$cleaned" #prefer local modified to generated
+        fi
+        $(improveMd "${text}" >> "$target")
+    done
+    cd "$DIR"
+}
+
+function improveMd()
+{
+    textIn="$1"
+    # sed1-3: fix frequent false positives from OCR
+    # sed4: remove hyphens at the end of the a line
+    # sed5: mark titles with a 'pos' style
+    # sed6: remove section brakes by wrong detected white spaces
+    cat "$textIn" \
+        | sed ':a;N;$!ba;s/-\n//g' \
+        | sed -E ':a;N;$!ba;s/([a-z,])\n\n([a-z])/\1 \2/g'
+}
+
 function bookToEPUB()
 {
+    MD_FIND='trans/*.md'
+    if [ ! -z "$1" ]; then
+        MD_FIND=$1
+    fi
+
     if ! [ -x "$(command -v pandoc)" ]; then
       sudo apt install -y pandoc
     fi
-    epub="$bookdir/$bookName.epub"
-    chapters=`ls -v $bookdir/*.md`
-    echo "creating $epub"
-    pandoc -o $epub $chapters
+    pandoc \
+     --epub-chapter-level=2 \
+     --epub-stylesheet="$WORK/style.css" \
+     -f markdown -o "$bookdir/$bookName.epub" \
+     "$WORK/meta.md" \
+     $(ls -v ${MD_FIND})
 }
 
 function bookToKindle()
